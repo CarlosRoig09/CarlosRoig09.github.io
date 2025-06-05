a825390e02a088941a91684cc44da1ec1ca5cc4e
Carlos Roig Garcia <carlos.roig.garcia9@gmail.com>
2025-06-05 19:02:03 +0200

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://carlosroig09.github.io/',
+    base: 'src/pages/index.astro'
+});
